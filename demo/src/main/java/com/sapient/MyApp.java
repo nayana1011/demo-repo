bcec19b16ea9628e129ab7462d9ebb6e97383814
nayn1 <nayn1@WKWZTcPOMXXevre.global.publicisgroupe.net>
2022-04-28 23:07:15 +0530

    commit

---
+++ demo/src/main/java/com/sapient/MyApp.java
@@ -0,0 +1,10 @@
+package com.sapient;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		System.out.println("MyApp");
+
+	}
+
+}
---
+++ demo/src/test/java/com/sapient/MyAppTest.java
@@ -0,0 +1,14 @@
+package com.sapient;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyAppTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
